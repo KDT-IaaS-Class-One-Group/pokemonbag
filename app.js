9074f15c062b8cdeff432359b0a550f3f77967bb
kimhyun <forhn37@gmail.com>
2023-10-26 17:55:12 +0900

    서버가동 문제 발생하여 재구성

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 데이터를 저장할 JSON 파일 경로
+// JSON 파일 경로 설정
 const dataFilePath = path.join(__dirname, 'data.json');
 
-// 초기 데이터 설정 (9개의 빈 문자열로 초기화)
-const initialData = new Array(9).fill('');
+// 정적 파일 제공 (public 폴더에 index.html과 sub.html이 있어야 함)
+app.use(express.static('public'));
 
-// JSON 파일에 초기 데이터를 저장
-fs.writeFileSync(dataFilePath, JSON.stringify(initialData));
+// JSON 파싱을 위한 미들웨어 설정
+app.use(bodyParser.json());
 
-// 클라이언트에서 데이터를 받아서 JSON 파일에 저장
-app.post('/sendData', (req, res) => {
-  const { textContent, index } = req.body;
+// 클라이언트에서 클릭으로 생성된 데이터를 JSON 파일에 저장
+app.post('/saveData', (req, res) => {
+  const newData = req.body;
 
-  // JSON 파일에서 데이터 읽기
-  const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+  // JSON 파일 읽기
+  const existingData = fs.readFileSync(dataFilePath, 'utf-8');
+  let jsonData = JSON.parse(existingData);
 
-  // 클라이언트에서 받은 데이터를 해당 인덱스에 저장
-  data[index] = textContent;
+  // 클라이언트에서 받은 데이터를 JSON에 추가
+  jsonData.push(newData);
 
   // JSON 파일에 업데이트된 데이터 저장
-  fs.writeFileSync(dataFilePath, JSON.stringify(data));
+  fs.writeFileSync(dataFilePath, JSON.stringify(jsonData));
 
   res.status(200).send('데이터가 저장되었습니다.');
 });
 
-// 서버에서 클라이언트로 데이터 전송
-app.get('/getData', (req, res) => {
+// 서브 페이지로 데이터 전송
+app.get('/sub.html', (req, res) => {
   // JSON 파일에서 데이터 읽기
-  const data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+  const existingData = fs.readFileSync(dataFilePath, 'utf-8');
+  const jsonData = JSON.parse(existingData);
 
-  res.json(data);
+  // sub.html 페이지로 데이터를 전송
+  res.json(jsonData);
 });
 
 app.listen(port, () => {
