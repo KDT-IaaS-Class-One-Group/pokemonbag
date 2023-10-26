595b6597856a5640a95ac5b8e4437f781dc2eb35
HongMoonGI <hmoongi94@gmail.com>
2023-10-26 16:59:32 +0900

    json데이터로 보내는 파일

---
+++ json.js
@@ -0,0 +1,20 @@
+// 데이터 객체 생성
+const dataToSend = {
+  message: '안녕하세요, 웹 페이지 2!'
+};
+
+// 버튼 클릭 이벤트 리스너
+const sendButton = document.getElementById('container');
+sendButton.addEventListener('click', () => {
+  // 데이터를 JSON 문자열로 변환
+  const jsonData = JSON.stringify(dataToSend);
+
+  // AJAX 또는 Fetch API를 사용하여 데이터를 백엔드 서버로 전송
+  fetch('/data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: jsonData
+  });
+});
