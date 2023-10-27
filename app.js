6c9e67932946d156556c0ad03608a565733fd440
YoonJunHyeon <qmdldos@gmail.com>
2023-10-27 15:35:56 +0900

    loadData 모듈 작성, 내용은 로딩됐을 때 요청한 데이터를 넣는다, 그리고 app.js에 라우팅 추가 /data, sub.html 동일화

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.get('/sub', (req, res) => {
   res.sendFile(__dirname + '/public/sub.html');
 });
 
+
+
+// * loadData전용 라우팅 /data 엔드폰이트로 작성된다.
+// * data.json을 읽도록 요청받은 것을 해결하여 데이터를 보낸다.
+
+app.get('/data', (req, res) => {
+  const dataPath = path.join(__dirname, './public/data/data.json');
+  const data = fs.readFileSync(dataPath, 'utf8');
+  res.send(data);
+});
+
+
+
 // 서버 시작
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
